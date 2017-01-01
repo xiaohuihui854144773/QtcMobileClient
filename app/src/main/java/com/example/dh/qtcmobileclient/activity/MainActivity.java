50faba66a54cafb368d5ec4f1022cca23521d544
DHH <854144773@qq.com>
2017-01-01 18:38:53 +0800

    首页  按钮实现

--- app/src/main/java/com/example/dh/qtcmobileclient/activity/MainActivity.java
+++ app/src/main/java/com/example/dh/qtcmobileclient/activity/MainActivity.java
@@ -3,32 +3,59 @@ package com.example.dh.qtcmobileclient.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.dh.qtcmobileclient.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private RadioGroup mRadio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+
+    private void initview() {
+        mRadio = (RadioGroup) findViewById(R.id.main_radio);
+        mRadio.setOnCheckedChangeListener(this);
     }
 
 
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int id) {
+        switch (id) {
+            case R.id.btn_menubar_home:
+
+                break;
+            case R.id.btn_menubar_news:
+
+                break;
+            case R.id.btn_menubar_zuinew:
+
+                break;
+            case R.id.btn_menubar_msg:
+
+                break;
+            case R.id.btn_menubar_user:
+
+                break;
+        }
+    }
+
     //双击退出
-    private long exitTime=0;
+    private long exitTime = 0;
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN)
-        {
-            if((System.currentTimeMillis()-exitTime) > 2000)
-            {
-                Toast.makeText(getApplicationContext(), "再按一次退出",Toast.LENGTH_SHORT).show();
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出", Toast.LENGTH_SHORT).show();
                 exitTime = System.currentTimeMillis();
-            }
-            else
-            {
+            } else {
                 finish();
                 System.exit(0);
             }
